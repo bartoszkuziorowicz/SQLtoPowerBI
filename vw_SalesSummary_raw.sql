696572a08fba8a0f0e6bc312be5d6ab24597b66a
bartoszkuziorowicz <127343485+bartoszkuziorowicz@users.noreply.github.com>
2025-08-19 16:13:40 +0200

    Raw View from AdventureWorks

---
+++ vw_SalesSummary_raw.sql
@@ -0,0 +1,33 @@
+﻿CREATE VIEW vw_SalesSummary_raw
+AS
+SELECT 
+    soh.SalesOrderID,
+    soh.OrderDate,
+    soh.CustomerID,
+    soh.OnlineOrderFlag,                 
+
+    sod.ProductID,
+    p.Name AS ProductName,
+    p.Color,
+    
+    psc.Name AS ProductSubcategory,      
+    pc.Name AS ProductCategory,         
+
+    sod.OrderQty,
+    sod.UnitPrice, 
+
+    st.CountryRegionCode AS CountryRegionCode   
+
+FROM Sales.SalesOrderHeader AS soh
+JOIN Sales.SalesOrderDetail AS sod
+  ON soh.SalesOrderID = sod.SalesOrderID
+JOIN Production.Product AS p
+  ON sod.ProductID = p.ProductID
+LEFT JOIN Production.ProductSubcategory AS psc
+  ON p.ProductSubcategoryID = psc.ProductSubcategoryID
+LEFT JOIN Production.ProductCategory AS pc
+  ON psc.ProductCategoryID = pc.ProductCategoryID
+LEFT JOIN Sales.SalesTerritory AS st
+  ON soh.TerritoryID = st.TerritoryID
+WHERE soh.OrderDate >= '2012-01-01';
+GO
